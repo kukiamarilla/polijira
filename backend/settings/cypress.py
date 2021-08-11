f44e3d07ed0cd5c134414d6331e1ba6982319179
Kuki Amarilla <sc.amarilla@gmail.com>
2021-08-10 21:44:37 -0400

    Configuracion de BD de Cypress

--- backend/settings/cypress.py
+++ backend/settings/cypress.py
@@ -3,9 +3,9 @@ from .dev import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test_django-vue-template',
-        'USER': 'django-vue-template',
-        'PASSWORD': 'django-vue-template',
+        'NAME': 'test_polijira',
+        'USER': 'polijira',
+        'PASSWORD': 'polijira',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
