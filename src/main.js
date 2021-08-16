b3142b337882d6a9848e1ffe8ded280963bba85d
Ramon Perdomo <rperdomorivas@gmail.com>
2021-08-15 22:06:48 -0400

    agregar login (90%). agregar navbar.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from '@/App.vue'
 import store from '@/store'
 import router from '@/router'
 
+import "@/assets/styles/main.css";
+
 Vue.config.productionTip = false
 
 // Vue.use(VueRouter)
