7f953b8faa1cba51810d4b018968f52c8bff0d80
Kuki Amarilla <sc.amarilla@gmail.com>
2021-08-16 00:33:48 -0400

    Configuracion de Firebase en Frontend

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import LottieVuePlayer from "@lottiefiles/vue-lottie-player";
+import * as firebase from "firebase";
 
 import App from '@/App.vue'
 import store from '@/store'
@@ -11,6 +12,17 @@ Vue.config.productionTip = false
 
 // Vue.use(VueRouter)
 Vue.use(LottieVuePlayer);
+
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const vue = new Vue({
   router,
