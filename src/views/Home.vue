1026aa6f7acc7689b807915a6afb71623a445b06
Ramon Perdomo <rperdomorivas@gmail.com>
2021-08-20 00:49:38 -0400

    agregar vista Home.

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Sidebar />
+
+    <h1 class="text-center">Aun no hay ningún proyecto creado. Crea uno.</h1>
+
+    <div class="d-flex justify-content-center">
+      <CardAdd class="big-add m-auto" />
+    </div>
+
+    <Waves class="waves" />
+  </div>
+</template>
+
+<script>
+import Sidebar from "@/components/Sidebar";
+import CardAdd from "@/components/CardAdd.vue";
+import Waves from "@/components/Waves";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Sidebar,
+    CardAdd,
+    Waves,
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.big-add,
+.waves {
+  margin-top: 120px;
+}
+</style>
