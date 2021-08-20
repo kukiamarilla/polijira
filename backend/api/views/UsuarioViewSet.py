88e7918aa5689ce1bf5111ff3ef4c44ea7243c87
Sebastian Cane <sebas.cane@hotmail.com>
2021-08-19 22:43:23 -0400

    Agregado obtener usuario en UsuarioViewSet

--- backend/api/views/UsuarioViewSet.py
+++ backend/api/views/UsuarioViewSet.py
@@ -32,3 +32,21 @@ class UsuarioViewSet(viewsets.ViewSet):
         usuarios = Usuario.objects.all()
         serializer = UsuarioSerializer(usuarios, many=True)
         return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        """
+        retrieve Obtiene un usuario mediante su pk
+
+        Args:
+            request (Any): request
+            pk (integer, opcional): primary key. Defaults to None.
+
+        Returns:
+            json: usuario obtenido en formato json
+        """
+        usuario = Usuario.objects.filter(pk=pk)
+        if usuario.count() == 0:
+            response = {"message": "No existe el usuario"}
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+        serializer = UsuarioSerializer(usuario[0], many=False)
+        return Response(serializer.data)
