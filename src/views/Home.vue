4e909fba51d41b9aec457ce3785d3ef50d1281e8
Ramon Perdomo <rperdomorivas@gmail.com>
2021-08-21 01:33:52 -0400

    alternar en Home entre ausencia y presencia de proyectos.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,15 @@
   <div>
     <Sidebar />
 
-    <h1 class="text-center">Aun no hay ningún proyecto creado. Crea uno.</h1>
+    <div v-if="proyectos" class="proyectos">
+      <ProyectoCard nombre="Proyecto 1" estado="Finalizado" />
+    </div>
+    <div v-else>
+      <h1 class="text-center">Aun no hay ningún proyecto creado. Crea uno.</h1>
 
-    <div class="d-flex justify-content-center">
-      <CardAdd class="big-add m-auto" />
+      <div class="d-flex justify-content-center">
+        <CardAdd class="big-add m-auto" />
+      </div>
     </div>
 
     <Waves class="waves" />
@@ -16,21 +21,29 @@
 import Sidebar from "@/components/Sidebar";
 import CardAdd from "@/components/CardAdd.vue";
 import Waves from "@/components/Waves";
+import ProyectoCard from "@/components/ProyectoCard";
 
 export default {
   data() {
-    return {};
+    return {
+      proyectos: [""],
+    };
   },
   components: {
     Sidebar,
     CardAdd,
     Waves,
+    ProyectoCard,
   },
   methods: {},
 };
 </script>
 
 <style scoped>
+.proyectos {
+  margin: 0 88px 120px 130px;
+}
+
 .big-add,
 .waves {
   margin-top: 120px;
