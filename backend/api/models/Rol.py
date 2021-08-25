1b91368095f68b8edda10d93e64371a1d3c7f878
Nerea Ortiz <nmoortiz@gmail.com>
2021-08-24 22:25:51 -0400

    Modificacion de Modelo de Rol para aplicar metodos agregar y eliminar permiso

--- backend/api/models/Rol.py
+++ backend/api/models/Rol.py
@@ -7,8 +7,27 @@ class Rol(models.Model):
     Modela la clase Rol
 
     Atributos:
-        nombre {CharField} -- nombre del rol
+        nombre {str} -- Nombre del rol
+        permisos {Permiso[]} -- Permisos del rol
     """
 
     nombre = models.CharField(max_length=255, default="")
     permisos = models.ManyToManyField(Permiso)
+
+    def agregar_permiso(self, permiso):
+        """
+        agregar_permiso Agrega un permiso al rol de sistema
+
+        Args:
+            permiso (Permiso): Permiso a agregar
+        """
+        self.permisos.add(permiso)
+
+    def eliminar_permiso(self, permiso):
+        """
+        eliminar_permiso Elimina un permiso de rol de sistema
+
+        Args:
+            permiso (Permiso): Permiso a eliminar
+        """
+        self.permisos.remove(permiso)
