365d3d6c0f9bd1395657063fbef98d73c37f965a
Ramon Perdomo <rperdomorivas@gmail.com>
2021-08-29 15:15:06 -0400

    actualizar tamaño de sidebar.

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -28,11 +28,10 @@ export default {
   box-shadow: 4px 0px 8px rgba(0, 0, 0, 0.125);
   display: flex;
   flex-direction: column;
-  height: 66vh;
+  height: calc(100% - 8rem - 75px);
   justify-content: space-between;
   left: 0;
   min-height: 300px;
-  max-height: 75vh;
   position: absolute;
   padding: 20px;
   top: 128px;
