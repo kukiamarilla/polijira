7ea569728b9c95842a21406d5d21011f88383a5c
Sebastian Cane <sebas.cane@hotmail.com>
2021-08-30 23:48:43 -0400

    Agregado la ruta para el modelo Proyecto

--- backend/urls.py
+++ backend/urls.py
@@ -10,8 +10,7 @@ from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
 from django.urls import path, include
 from rest_framework import routers
-from backend.api.views import UsuarioViewSet
-from backend.api.views import PermisoViewSet
+from backend.api.views import UsuarioViewSet, PermisoViewSet, ProyectoViewSet
 
 
 index_view = never_cache(TemplateView.as_view(template_name='index.html'))
@@ -20,6 +19,7 @@ router = routers.DefaultRouter()
 router.register("usuarios", UsuarioViewSet, basename="usuarios")
 router.register("permisos", PermisoViewSet, basename="permisos")
 router.register("roles", RolViewSet, basename="roles")
+router.register("proyectos", ProyectoViewSet, basename="proyectos")
 
 urlpatterns = [
 
