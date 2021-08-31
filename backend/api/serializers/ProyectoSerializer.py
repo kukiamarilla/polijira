67b443f628b4c54e9ec8f301b8084dd47493fb79
Sebastian Cane <sebas.cane@hotmail.com>
2021-08-30 23:45:43 -0400

    Actualizado el serializer para visualizar al scrum master

--- backend/api/serializers/ProyectoSerializer.py
+++ backend/api/serializers/ProyectoSerializer.py
@@ -1,3 +1,4 @@
+from backend.api.serializers import UsuarioSerializer
 from rest_framework import serializers
 from backend.api.models import Proyecto
 
@@ -6,10 +7,11 @@ class ProyectoSerializer(serializers.ModelSerializer):
     """
     ProyectoSerializer Serializer del modelo Proyecto
     """
+    scrum_master = UsuarioSerializer(many=False, read_only=True)
 
     class Meta:
         """
          Metadatos de ProyectoSerializer
         """
         model = Proyecto
-        fields = ('id', 'nombre', 'fecha_inicio', 'fecha_fin', 'estado')
+        fields = ('id', 'nombre', 'fecha_inicio', 'fecha_fin', 'estado', 'scrum_master')
