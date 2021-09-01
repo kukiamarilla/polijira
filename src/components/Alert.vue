3e220beff666a79a383fd1b10ead6ec15940e65c
Ramon Perdomo <rperdomorivas@gmail.com>
2021-09-01 01:07:52 -0400

    agregar componente Alert.

---
+++ src/components/Alert.vue
@@ -0,0 +1,57 @@
+<template>
+  <Modal :show="true" height="387px" width="735px">
+    <div class="alert-container">
+      <h2>{{ title }}</h2>
+
+      <Icon :icono="icon" :color="iconColor" size="105px" />
+
+      <div class="highlight">{{ message }}</div>
+
+      <Boton texto="Aceptar" tema="primary" />
+    </div>
+  </Modal>
+</template>
+
+<script>
+import Modal from "@/components/Modal";
+import Icon from "@/components/Icon";
+import Boton from "@/components/Boton";
+
+export default {
+  props: { show: Boolean, message: String, type: String },
+  computed: {
+    title() {
+      return this.type === "success" ? "Éxito" : "Error";
+    },
+    icon() {
+      return this.type === "success" ? "success" : "warning";
+    },
+    iconColor() {
+      return this.type === "success" ? "var(--success)" : "var(--danger)";
+    },
+  },
+  components: {
+    Modal,
+    Icon,
+    Boton,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.alert-container {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+
+h2 {
+  align-self: flex-start;
+  margin-bottom: 28px;
+}
+
+.highlight {
+  margin-bottom: 64px;
+  margin-top: 28px;
+}
+</style>
