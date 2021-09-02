bb709ff189eafb94badb98479c33f5dd2107ffd1
Ramon Perdomo <rperdomorivas@gmail.com>
2021-09-01 23:58:41 -0400

    agregar comentario con descripcion de parametro para cargar alerta.

--- src/store/alert.js
+++ src/store/alert.js
@@ -8,6 +8,12 @@ const alert = {
   namespaced: true,
   state: { alert: INITIAL_STATE },
   mutations: {
+    /**
+     * @param payload is an object with two attributes:
+     * -message: an string that the alert will show
+     * -type: has two values, 'success' and 'error'.
+     *        decide what icon will be shwon.
+     */
     showAlert(state, payload) {
       state.alert = { show: true, ...payload };
     },
