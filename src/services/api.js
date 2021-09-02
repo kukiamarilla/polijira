cd1a68160ebc368d7494834e88ea20ace56e19f2
Kuki Amarilla <sc.amarilla@gmail.com>
2021-09-02 17:41:47 -0400

    Mensajes de error de consultas

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
+import store from '@/store'
 
-export default axios.create({
+let api = axios.create({
   baseURL: '/api',
   timeout: 5000,
   headers: {
@@ -9,3 +10,13 @@ export default axios.create({
     'X-CSRFToken': Cookies.get('csrftoken')
   }
 })
+
+api.interceptors.response.use((response) => response, (error) => {
+  if(error.response)
+    store.commit("alert/showAlert", {type: "error", message: error.response.data.message})
+  else 
+    store.commit("alert/showAlert", {type: "error", message: "No se pudo conectar con el servidor"})
+  throw error;
+});
+
+export default api;
