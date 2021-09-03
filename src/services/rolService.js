cef1ed0ca009fa0f4e10f826f0c70441bacca90c
Kuki Amarilla <sc.amarilla@gmail.com>
2021-09-02 20:47:25 -0400

    Refactor de crear rol en frontend

--- src/services/rolService.js
+++ src/services/rolService.js
@@ -3,5 +3,8 @@ import api from './api'
 export default {
     list() {
         return api.get("/roles/").then(response => response.data)
+    },
+    create(data) {
+        return api.post("/roles/", data).then(response => response.data)
     }
 }
