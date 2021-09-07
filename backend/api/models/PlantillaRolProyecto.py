90dbb466a271bc5aab0b634e4a52bbff28dc565c
Sebastian Cane <sebas.cane@hotmail.com>
2021-09-06 23:35:06 -0400

    Creado el modelo PlantillaRolProyecto

---
+++ backend/api/models/PlantillaRolProyecto.py
@@ -0,0 +1,35 @@
+from django.db import models
+from .PermisoProyecto import PermisoProyecto
+
+
+class PlantillaRolProyecto(models.Model):
+    """
+    PlantillaRolProyecto Modelo los roles de Proyecto en formato Plantilla
+
+    Args:
+        models (Model): Modelo de Django
+
+    Atributes:
+        nombre (CharField): Nombre del rol
+        permisos (ManyToManyField): Permisos del rol
+    """
+    nombre = models.CharField(max_length=255, default="")
+    permisos = models.ManyToManyField(PermisoProyecto)
+
+    def agregar_permiso(self, permiso):
+        """
+        agregar_permiso Agrega un permiso de proyecto a este rol
+
+        Args:
+            permiso (PermisoProyecto): Permiso de Proyecto
+        """
+        self.permisos.add(permiso)
+
+    def eliminar_permiso(self, permiso):
+        """
+        eliminar_permiso Elimina un permiso de proyecto a este rol
+
+        Args:
+            permiso (PermisoProyecto): Permiso de Proyecto
+        """
+        self.permisos.remove(permiso)
