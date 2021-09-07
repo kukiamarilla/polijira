a063d1eef5847f088bff59b5280feb96af4afd34
Sebastian Cane <sebas.cane@hotmail.com>
2021-09-06 23:36:26 -0400

    Agregado al init los nuevos modelos

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -2,3 +2,5 @@ from .Usuario import Usuario
 from .Permiso import Permiso
 from .Rol import Rol
 from .Proyecto import Proyecto
+from .PermisoProyecto import PermisoProyecto
+from .PlantillaRolProyecto import PlantillaRolProyecto
