ef0bcaa153bd6c2ee923241a31d14efe44afa63a
Sebastian Cane <sebas.cane@hotmail.com>
2021-09-09 23:21:34 -0400

    Actualizado urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -4,8 +4,6 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
 """
 
-from backend.api.models.PlantillaRolProyecto import PlantillaRolProyecto
-from backend.api.views.RolViewSet import RolViewSet
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
@@ -14,8 +12,10 @@ from rest_framework import routers
 from backend.api.views import \
     UsuarioViewSet, \
     PermisoViewSet, \
+    RolViewSet, \
     ProyectoViewSet, \
-    PlantillaRolProyectoViewSet
+    PlantillaRolProyectoViewSet, \
+    PermisoProyectoViewSet
 
 
 index_view = never_cache(TemplateView.as_view(template_name='index.html'))
@@ -26,6 +26,7 @@ router.register("permisos", PermisoViewSet, basename="permisos")
 router.register("roles", RolViewSet, basename="roles")
 router.register("proyectos", ProyectoViewSet, basename="proyectos")
 router.register("plantillas", PlantillaRolProyectoViewSet, basename="plantillas")
+router.register("permisos-proyecto", PermisoProyectoViewSet, basename="permisos-proyecto")
 
 urlpatterns = [
 
