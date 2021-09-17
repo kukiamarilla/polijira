fcf02d4edabd9dd2e4792f659e38212ffcd8a2b5
Sebastian Cane <sebas.cane@hotmail.com>
2021-09-16 23:39:29 -0400

    Agregado metodos estaticos para manejar miembros al crear un proyecto

--- backend/api/models/Miembro.py
+++ backend/api/models/Miembro.py
@@ -1,5 +1,5 @@
 from django.db import models
-from backend.api.models import Usuario, PermisoProyecto, RolProyecto, Proyecto
+from backend.api.models import PermisoProyecto, RolProyecto
 
 
 class Miembro(models.Model):
@@ -15,9 +15,9 @@ class Miembro(models.Model):
         rol (ForeignKey): Rol de proyecto del miembro en el proyecto
     """
 
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='miembros')
-    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE, related_name='miembros')
-    rol = models.ForeignKey(RolProyecto, on_delete=models.CASCADE, related_name='miembros')
+    usuario = models.ForeignKey("Usuario", on_delete=models.CASCADE, related_name='miembros')
+    proyecto = models.ForeignKey("Proyecto", on_delete=models.CASCADE, related_name='miembros')
+    rol = models.ForeignKey("RolProyecto", on_delete=models.CASCADE, related_name='miembros')
 
     def tiene_permiso(self, permiso_codigo):
         """
@@ -34,3 +34,29 @@ class Miembro(models.Model):
             return True
         except PermisoProyecto.DoesNotExist:
             return False
+
+    @staticmethod
+    def asignar_scrum_master(proyecto, scrum_master):
+        proyecto.scrum_master = scrum_master
+        proyecto.save()
+        Miembro.objects.create(
+            usuario=proyecto.scrum_master,
+            proyecto=proyecto,
+            rol=RolProyecto.objects.get(nombre="Scrum Master")
+        )
+
+    @staticmethod
+    def actualizar_scrum_master(proyecto, scrum_master):
+        rol = RolProyecto.objects.get(nombre="Scrum Master")
+        miembro = Miembro.objects.get(
+            usuario=proyecto.scrum_master,
+            proyecto=proyecto,
+            rol=rol
+        )
+        miembro.delete()
+        proyecto.update(scrum_master=scrum_master)
+        Miembro.objects.create(
+            usuario=scrum_master,
+            proyecto=proyecto,
+            rol=rol
+        )
