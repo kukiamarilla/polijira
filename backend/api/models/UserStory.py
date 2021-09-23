4317aae81650b235a94e294a879bb778b5c8dd11
Sebastian Cane <sebas.cane@hotmail.com>
2021-09-22 23:21:33 -0400

    Agregado el related_name al atributo desarrollador

--- backend/api/models/UserStory.py
+++ backend/api/models/UserStory.py
@@ -8,6 +8,23 @@ ESTADOS = (
 
 
 class UserStory(models.Model):
+    """
+    UserStory Modela la clase User Story
+
+    Args:
+        models (Model): Modelo de Django
+
+    Atributes:
+        nombre (CharField): Nombre del User Story
+        descripcion (TextField): Descripcion del User Story
+        horas_estimadas (IntegerField): Hora estimada del User Story
+        prioridad (IntegerField): Prioridad del User Story
+        estado (CharField): Estado actual de User Story
+        fecha_release (DateField): Fecha en que se libera el User Story
+        fecha_creacion (DateField): Fecha en que se crea el User Story
+        desarrollador (ForeignKey): El miembro que desarrolla el User Story
+        estado_estimacion (CharField): 
+    """
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
     horas_estimadas = models.IntegerField()
@@ -15,7 +32,7 @@ class UserStory(models.Model):
     estado = models.CharField(max_length=1, choices=ESTADOS, default="P")
     fecha_release = models.DateField()
     fecha_creacion = models.DateField()
-    desarrollador = models.ForeignKey("Miembro", on_delete=models.CASCADE)
+    desarrollador = models.ForeignKey("Miembro", on_delete=models.CASCADE, related_name="user_stories")
     estado_estimacion = models.CharField(max_length=1)
     product_backlog = models.BooleanField()
 
