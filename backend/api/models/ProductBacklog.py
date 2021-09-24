c2cc1fa06abe64837161e639ed71e285c02077c7
Sebastian Cane <sebas.cane@hotmail.com>
2021-09-24 02:03:21 -0400

    Creado el modelo Product Backlog

---
+++ backend/api/models/ProductBacklog.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class ProductBacklog(models.Model):
+    proyecto = models.OneToOneField("Proyecto", on_delete=models.CASCADE, related_name="product_backlogs")
+    user_story = models.ForeignKey("UserStory", on_delete=models.CASCADE, related_name="product_backlogs")
+
+    @staticmethod
+    def almacenar_user_story(user_story, proyecto):
+        ProductBacklog.objects.create(
+            proyecto=proyecto,
+            user_story=user_story
+        )
+        user_story.product_backlog = True
+        user_story.save()
