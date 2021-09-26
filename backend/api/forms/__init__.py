4fc6fbfc1267e24f1fe5a7658c2d266239ef131f
Sebastian Cane <sebas.cane@hotmail.com>
2021-09-26 03:28:53 -0400

    Creado el form para validar las modificaciones de un User Story

--- backend/api/forms/__init__.py
+++ backend/api/forms/__init__.py
@@ -14,3 +14,4 @@ from .UpdateHorarioForm import UpdateHorarioForm
 from .CreateMiembroForm import CreateMiembroForm
 from .UpdateMiembroForm import UpdateMiembroForm
 from .CreateUserStoryForm import CreateUserStoryForm
+from .UpdateUserStoryForm import UpdateUserStoryForm
