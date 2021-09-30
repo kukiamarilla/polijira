90b7f1cd5a8078e9953eaacb4a08fd480f8a4b67
Ramon Perdomo <rperdomorivas@gmail.com>
2021-09-29 22:56:23 -0400

    evitar sobreescribir estado del alert.

--- src/store/alert.js
+++ src/store/alert.js
@@ -10,12 +10,12 @@ const alert = {
   mutations: {
     /**
      * @param payload is an object with two attributes:
-     * -message: an string that the alert will show
-     * -type: has two values, 'success' and 'error'.
-     *        decide what icon will be shwon.
+     * -message: an string that the alert will show.
+     * -type: has two values, 'success' and 'error',
+     *        decide what icon will be shown.
      */
     showAlert(state, payload) {
-      state.alert = { show: true, ...payload };
+      state.alert = { ...payload, show: true };
     },
     hideAlert(state) {
       state.alert = { ...INITIAL_STATE };
