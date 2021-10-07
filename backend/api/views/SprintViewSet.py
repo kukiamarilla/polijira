dbc63d00acf1fa4aaa2e468b81159ed49a2ce65b
Sebastian Cane <sebas.cane@hotmail.com>
2021-10-07 05:11:29 -0300

    Agregado servicio de Ver detalles de un Sprint

--- backend/api/views/SprintViewSet.py
+++ backend/api/views/SprintViewSet.py
@@ -13,8 +13,55 @@ class SprintViewSet(viewsets.ViewSet):
         viewsets (ViewSet): View del módulo rest_framework
     """
 
+    def retrieve(self, request, pk=None):
+        """
+        retrieve Obtiene los detalles de un Sprint
+
+        Args:
+            request (Any): Request que se solicita
+            pk (int, optional): Primary key
+
+        Returns:
+            JSON: Metados del Sprint
+        """
+        try:
+            usuario = Usuario.objects.get(user=request.user)
+            sprint = Sprint.objects.get(pk=pk)
+            miembro = Miembro.objects.get(usuario=usuario, proyecto=sprint.planificador.proyecto)
+            if not miembro.tiene_permiso("ver_sprints"):
+                response = {
+                    "message": "No tiene permiso para realizar esta acción",
+                    "permission_required": ["ver_sprints"],
+                    "error": "forbidden"
+                }
+                return Response(response, status=status.HTTP_403_FORBIDDEN)
+            serializer = SprintSerializer(sprint, many=False)
+            return Response(serializer.data)
+        except Sprint.DoesNotExist:
+            response = {
+                "message": "No existe el Sprint",
+                "error": "not_found"
+            }
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+        except Miembro.DoesNotExist:
+            response = {
+                "message": "Usted no es miembro de este proyecto",
+                "error": "forbidden"
+            }
+            return Response(response, status=status.HTTP_403_FORBIDDEN)
+
     @action(detail=True, methods=["POST"])
     def iniciar(self, request, pk=None):
+        """
+        iniciar Inicia un Sprint Planning
+
+        Args:
+            request (Any): Request que se solicita
+            pk (int, optional): Primary key
+
+        Returns:
+            JSON: Metados del Sprint que se inició el Sprint Planning
+        """
         try:
             usuario = Usuario.objects.get(user=request.user)
             sprint = Sprint.objects.get(pk=pk)
