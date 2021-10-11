b91ee1b82c9cd39976866aee2bffd28be6cd9654
Kuki Amarilla <sc.amarilla@gmail.com>
2021-10-11 03:18:02 -0300

    Creacion de User Story

--- src/services/userStoryService.js
+++ src/services/userStoryService.js
@@ -2,6 +2,9 @@ import api from './api'
 
 export default {
     list(idProyecto) {
-        return api.get(`/proyectos/${idProyecto}/user_stories`).then(response => response.data)
+        return api.get(`/proyectos/${idProyecto}/user_stories/`).then(response => response.data)
+    },
+    create(userStory) {
+        return api.post(`/user-stories/`, userStory).then(response => response.data)
     }
 }
