870d96f87672e293fa40355239918700b5f2ace4
Nerea Ortiz <nmoortiz@gmail.com>
2021-10-11 03:12:52 -0300

    Agregado metodos de incluir/eliminar miembro a sprint (paso 1)

--- backend/api/views/SprintPlanningViewSet.py
+++ backend/api/views/SprintPlanningViewSet.py
@@ -3,9 +3,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.db import transaction
 from backend.api.models import Miembro, MiembroSprint, ProductBacklog, Sprint, SprintBacklog, UserStory, Usuario
-from backend.api.serializers import SprintSerializer
+from backend.api.serializers import SprintSerializer, MiembroSprintSerializer
 from backend.api.decorators import FormValidator
-from backend.api.forms import PlanificarUserStoryForm
+from backend.api.forms import PlanificarUserStoryForm, CreateMiembroSprintForm, EliminarMiembroSprintForm
 
 
 class SprintPlanningViewSet(viewsets.ViewSet):
@@ -71,6 +71,172 @@ class SprintPlanningViewSet(viewsets.ViewSet):
                 "error": "forbidden"
             }
             return Response(response, status=status.HTTP_403_FORBIDDEN)
+
+    @action(detail=True, methods=["GET"])
+    def miembros(self, request, pk=None):
+        """
+        miembros Lista los miembros de un sprint
+
+        Args:
+            request (Any): request
+            pk (integer, optional): primary key. Defaults to None.
+        """
+        try:
+            usuario = Usuario.objects.get(user=request.user)
+            sprint = Sprint.objects.get(pk=pk)
+            miembro = Miembro.objects.get(usuario=usuario, proyecto=sprint.proyecto)
+            if not (miembro.tiene_permiso("ver_sprints") and
+                    miembro.tiene_permiso("ver_miembros") and
+                    miembro.tiene_permiso("ver_miembros_sprint")):
+                response = {
+                    "message": "No tiene permiso para realizar esta acción",
+                    "permission_required": [
+                        "ver_sprints",
+                        "ver_miembros",
+                        "ver_miembros_sprint"
+                    ],
+                    "error": "forbidden"
+                }
+                return Response(response, status=status.HTTP_403_FORBIDDEN)
+            miembro_sprint = MiembroSprint.objects.filter(sprint=sprint)
+            serializer = MiembroSprintSerializer(miembro_sprint, many=True)
+            return Response(serializer.data)
+        except Miembro.DoesNotExist:
+            response = {
+                "message": "Usted no es miembro de este proyecto",
+                "error": "forbidden"
+            }
+            return Response(response, status=status.HTTP_403_FORBIDDEN)
+        except Sprint.DoesNotExist:
+            response = {
+                "message": "Sprint especificado no existe",
+                "error": "not_found"
+            }
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+
+    @transaction.atomic
+    @FormValidator(form=CreateMiembroSprintForm)
+    @miembros.mapping.post
+    def agregar_miembro(self, request, pk=None):
+        """
+        agregar_miembro Agrega un miembro de un sprint
+
+        Args:
+            request (Any): request
+
+        Returns:
+            JSON: Miembro de sprint agregado
+        """
+        try:
+            usuario = Usuario.objects.get(user=request.user)
+            sprint = Sprint.objects.get(pk=pk)
+            proyecto = sprint.proyecto
+            miembro_request = Miembro.objects.get(usuario=usuario, proyecto=proyecto)
+            if sprint.estado_sprint_planning != 'I':
+                response = {
+                    "message": "Planificación del sprint no fue iniciada",
+                    "error": "forbidden"
+                }
+                return Response(response, status=status.HTTP_403_FORBIDDEN)
+            if sprint.planificador != miembro_request:
+                response = {
+                    "message": "Usted no es el planificador de sprint",
+                    "error": "forbidden"
+                }
+                return Response(response, status=status.HTTP_403_FORBIDDEN)
+            miembro = Miembro.objects.get(pk=request.data["miembro"])
+            if miembro.proyecto != proyecto:
+                response = {
+                    "message": "El miembro no pertenece a este proyecto",
+                    "error": "bad_request"
+                }
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            sprint_backlog = SprintBacklog.objects.filter(sprint=sprint)
+            if len(sprint_backlog) > 0:
+                response = {
+                    "message": "Este paso ya se completó",
+                    "error": "bad_request"
+                }
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            miembro_sprint = MiembroSprint.objects.filter(miembro_proyecto=miembro, sprint=sprint)
+            if len(miembro_request) > 0:
+                response = {
+                    "message": "Este miembro ya fue agregado al Sprint",
+                    "error": "bad_request"
+                }
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            miembro_sprint = MiembroSprint.objects.create(miembro_proyecto=miembro, sprint=sprint)
+            serializer = MiembroSprintSerializer(miembro_sprint, many=False)
+            return Response(serializer.data)
+        except Miembro.DoesNotExist:
+            response = {
+                "message": "Usted no es miembro de este proyecto",
+                "error": "forbidden"
+            }
+            return Response(response, status=status.HTTP_403_FORBIDDEN)
+        except Sprint.DoesNotExist:
+            response = {
+                "message": "No existe el Sprint especificado",
+                "error": "not_found"
+            }
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+
+    @FormValidator(form=EliminarMiembroSprintForm)
+    @miembros.mapping.delete
+    def eliminar_miembros(self, request, pk=None):
+        """
+        destroy Elimina un miembro de un sprint
+
+        Args:
+            request (Any): request
+            pk (integer, opcional): Primary Key del miembro del sprint a eliminar
+        """
+        try:
+            usuario = Usuario.objects.get(user=request.user)
+            sprint = Sprint.objects.get(pk=pk)
+            proyecto = sprint.proyecto
+            miembro_request = Miembro.objects.get(usuario=usuario, proyecto=proyecto)
+            if sprint.estado_sprint_planning != 'I':
+                response = {
+                    "message": "Planificación del sprint no fue iniciada",
+                    "error": "forbidden"
+                }
+                return Response(response, status=status.HTTP_403_FORBIDDEN)
+            if sprint.planificador != miembro_request:
+                response = {
+                    "message": "Usted no es el planificador de sprint",
+                    "error": "forbidden"
+                }
+                return Response(response, status=status.HTTP_403_FORBIDDEN)
+            miembro_sprint = MiembroSprint.objects.get(pk=request.data["miembro_sprint"])
+            if miembro_sprint.sprint != sprint:
+                response = {
+                    "message": "El miembro sprint no pertenece a este sprint",
+                    "error": "bad_request"
+                }
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            sprint_backlog = SprintBacklog.objects.filter(sprint=sprint)
+            if len(sprint_backlog) > 0:
+                response = {
+                    "message": "Este paso ya se completó",
+                    "error": "bad_request"
+                }
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            miembro_sprint.delete()
+            response = {"message": "Miembro Sprint eliminado."}
+            return Response(response)
+        except Miembro.DoesNotExist:
+            response = {
+                "message": "Usted no es miembro de este proyecto",
+                "error": "forbidden"
+            }
+            return Response(response, status=status.HTTP_403_FORBIDDEN)
+        except Sprint.DoesNotExist:
+            response = {
+                "message": "No existe el Sprint especificado",
+                "error": "not_found"
+            }
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
 
     @transaction.atomic
     @action(detail=True, methods=["POST"])
