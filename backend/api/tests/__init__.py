efe44eeb5d526763ee81347b782259d08f7108d3
Nerea Ortiz <nmoortiz@gmail.com>
2021-10-10 21:55:12 -0300

    Test MiembroSprint

--- backend/api/tests/__init__.py
+++ backend/api/tests/__init__.py
@@ -9,3 +9,4 @@ from .MiembroTestCase import MiembroTestCase
 from .HorarioTestCase import HorarioTestCase
 from .UserStoryTestCase import UserStoryTestCase
 from .SprintTestCase import SprintTestCase
+from .MiembroSprintTestCase import MiembroSprintTestCase
