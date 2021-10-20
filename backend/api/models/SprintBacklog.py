ff18a74ef6ff4da90adeca9d3f905329a57874e6
Nerea Ortiz <nmoortiz@gmail.com>
2021-10-20 19:26:32 -0300

    Agregado atributo estado_kanban a modelo SprintBacklog

--- backend/api/models/SprintBacklog.py
+++ backend/api/models/SprintBacklog.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+ESTADOS_KANBAN = (
+    ("T", "Todo"),
+    ("D", "Doing"),
+    ("N", "Done")
+)
+
 
 class SprintBacklog(models.Model):
     """
@@ -14,7 +20,15 @@ class SprintBacklog(models.Model):
     """
     sprint = models.ForeignKey("Sprint", on_delete=models.CASCADE, related_name="sprint_backlogs")
     user_story = models.ForeignKey("UserStory", on_delete=models.CASCADE, related_name="sprint_backlogs")
+    estado_kanban = models.CharField(max_length=1, choices=ESTADOS_KANBAN, default="T")
 
     @staticmethod
     def agregar_user_story(sprint=None, user_story=None):
         SprintBacklog.objects.create(sprint=sprint, user_story=user_story)
+
+    def mover_kanban(self, estado):
+        """
+        mover Mueve el user story a la siguiente columna del Kanban
+        """
+        self.estado_kanban = estado
+        self.save()
