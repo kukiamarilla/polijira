c16878fdb7f7566fb6a46e303152c2afedf1f849
Nerea Ortiz <nmoortiz@gmail.com>
2021-10-21 23:48:57 -0300

    Modelo Review

---
+++ backend/api/models/Review.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Review(models.Model):
+    """
+    Review
+    Modela el Review de User Stories
+
+    Args:
+        models (Model): Model de django
+
+    Atributes:
+        user_story (ForeignKey): User Story al que hace referencia el review
+        observacion (TextField): Descripcion sobre el del User Story
+        autor (ForeignKey): Miembro del proyecto que hace el review
+    """
+    user_story = models.ForeignKey("UserStory", on_delete=models.CASCADE, related_name="reviews")
+    observacion = models.TextField(default="")
+    fecha_creacion = models.DateField()
+    autor = models.ForeignKey("Usuario", on_delete=models.CASCADE, related_name="reviews")
