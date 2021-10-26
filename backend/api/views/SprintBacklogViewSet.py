2b5cdea500ee5a272b02f98f4974be51fb2378b1
Sebastian Cane <sebas.cane@hotmail.com>
2021-10-25 23:26:55 -0300

    Movido servicio responder estimacion a Sprint Backlog ViewSet

---
+++ backend/api/views/SprintBacklogViewSet.py
@@ -0,0 +1,67 @@
+from rest_framework import status, views
+from django.db import transaction
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from backend.api.decorators import FormValidator
+from backend.api.forms import ResponderEstimacionForm
+from backend.api.models import Miembro, SprintBacklog, Usuario, RegistroUserStory
+from backend.api.serializers import SprintBacklogSerializer
+
+
+class SprintBacklogViewSet(views.View):
+
+    @transaction.atomic
+    @action(detail=True, methods=["POST"])
+    @FormValidator(form=ResponderEstimacionForm)
+    def responder_estimacion(self, request, pk=None):
+        try:
+            usuario = Usuario.objects.get(user=request.user)
+            sprint_backlog = SprintBacklog.objects.get(pk=pk)
+            sprint = sprint_backlog.sprint
+            miembro = Miembro.objects.get(usuario=usuario, proyecto=sprint.proyecto)
+            user_story = sprint_backlog.user_story
+            if not sprint.estado_sprint_planning == "I":
+                response = {
+                    "message": "Un Planificador debe Iniciar el Sprint Planning para responder una estimación",
+                    "error": "bad_request"
+                }
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            if not user_story.estado_estimacion == "p":
+                response = {
+                    "message": "Este User Story aun no fue estimado por el Planificador",
+                    "error": "bad_request"
+                }
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            if not miembro == user_story.desarrollador.miembro_proyecto:
+                response = {
+                    "message": "Usted no es desarrollador de este User Story",
+                    "error": "bad_request"
+                }
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            if not sprint.estado == "P":
+                response = {
+                    "message": "Solo puedes responder una estimación de un Sprint Pendiente",
+                    "error": "conflict"
+                }
+                return Response(response, status=status.HTTP_409_CONFLICT)
+            user_story.update(
+                horas_estimadas=(int(request.data.get("horas_estimadas")) + int(user_story.horas_estimadas))/2,
+                estado_estimacion="C",
+                autor=miembro,
+                registro_handler=RegistroUserStory.modificar_registro
+            )
+            serializer = SprintBacklogSerializer(sprint_backlog, many=False)
+            return Response(serializer.data)
+        except SprintBacklog.DoesNotExist:
+            response = {
+                "message": "No existe el Sprint Backlog",
+                "error": "not_found"
+            }
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+        except Miembro.DoesNotExist:
+            response = {
+                "message": "Usted no es miembro de este Proyecto",
+                "error": "forbidden"
+            }
+            return Response(response, status=status.HTTP_403_FORBIDDEN)
