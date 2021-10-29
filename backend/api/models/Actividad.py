7a04abd691b2e87b3efad715abc567ec6629a28b
Sebastian Cane <sebas.cane@hotmail.com>
2021-10-29 02:04:17 -0300

    Creado el modelo Actividad

---
+++ backend/api/models/Actividad.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Actividad(models.Model):
+    """
+    Actividad Modela las actividades de un User Story
+
+    Args:
+        models (Model): Modelo del módulo Django
+    """
+
+    descripcion = models.TextField(default="")
+    horas = models.IntegerField(default=0)
+    fecha_creacion = models.DateField()
+    user_story = models.ForeignKey("UserStory", on_delete=models.CASCADE)
