8f280fbae0c10db6f50973a8b8671c24da477ae6
Nerea Ortiz <nmoortiz@gmail.com>
2021-10-29 03:37:04 -0300

    Debugging para testing de pipeline

--- backend/api/tests/SprintBacklogTestCase.py
+++ backend/api/tests/SprintBacklogTestCase.py
@@ -55,6 +55,7 @@ class SprintBacklogTestCase(TestCase):
         }
         response = self.client.post("/api/sprint-backlogs/" + str(sprint_backlog.pk) + "/mover/",
                                     request_data, content_type="application/json")
+        print(response.content)
         body = response.json()
         self.assertEquals(response.status_code, 200)
         self.assertEquals(body["sprint"]["id"], sprint_backlog.sprint.pk)
