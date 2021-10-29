6dec54d7312cfe9c294049e071dd574fe3170c93
Ramon Perdomo <rperdomorivas@gmail.com>
2021-10-29 06:50:05 -0300

    agregar servicio para ver product backlog.

--- src/services/proyectoService.js
+++ src/services/proyectoService.js
@@ -11,17 +11,22 @@ export default {
     return api.post("/proyectos/", data).then((response) => response.data);
   },
   update(id, data) {
-    return api
-      .put(`/proyectos/${id}/`, data)
-      .then((response) => response.data);
+    return api.put(`/proyectos/${id}/`, data).then((response) => response.data);
   },
   delete(id) {
     return api.delete(`/proyectos/${id}/`).then((response) => response.data);
   },
   activar(id) {
-    return api.post(`/proyectos/${id}/activar/`).then((response) => response.data);
+    return api
+      .post(`/proyectos/${id}/activar/`)
+      .then((response) => response.data);
   },
   me(id) {
     return api.get(`/proyectos/${id}/me/`).then((response) => response.data);
-  }
+  },
+  backlog(id) {
+    return api
+      .get(`/proyectos/${id}/product_backlogs/`)
+      .then((response) => response.data);
+  },
 };
