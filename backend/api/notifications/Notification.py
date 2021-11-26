d6aee440aa08c08b2d4a305c4136a6c3e39a2cb9
Sebastian Cane <sebas.cane@hotmail.com>
2021-11-26 14:32:54 -0300

    Agregado doc a Notification

--- backend/api/notifications/Notification.py
+++ backend/api/notifications/Notification.py
@@ -3,7 +3,14 @@ from django.template.loader import render_to_string
 from django.conf import settings
 
 
-class Notification:
+class Notification():
+    """
+    Notification Modela las notificaciones que reciben los usuarios
+
+    Atributes:
+        template (str): template a enviar
+        subject (str): subject a enviar
+    """
     template = ""
     subject = ""
 
@@ -11,9 +18,21 @@ class Notification:
         self.data = data
 
     def build_message(self):
+        """
+        build_message Convierte a String el dato a enviar
+
+        Returns:
+            str: dato a str
+        """
         return render_to_string(self.template, self.data)
 
     def notify_all(self, usuarios):
+        """
+        notify_all Notifica a varios usuarios
+
+        Args:
+            usuarios (list Usuario): Lista de Usuarios
+        """
         emails = list(map(lambda usuario: usuario.email, usuarios))
         message = self.build_message()
         send_mail(self.subject, message, settings.EMAIL_HOST_USER, emails, html_message=message)
