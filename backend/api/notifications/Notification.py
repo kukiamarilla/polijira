c9bd54597f6c46781992cf6000e18bdb9c81559e
Sebastian Cane <sebas.cane@hotmail.com>
2021-11-26 14:20:02 -0300

    Agregado metodo para notificar a todos los usuarios

--- backend/api/notifications/Notification.py
+++ backend/api/notifications/Notification.py
@@ -1,4 +1,6 @@
+from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.conf import settings
 
 
 class Notification:
@@ -10,3 +12,8 @@ class Notification:
 
     def build_message(self):
         return render_to_string(self.template, self.data)
+
+    def notify_all(self, usuarios):
+        emails = list(map(lambda usuario: usuario.email, usuarios))
+        message = self.build_message()
+        send_mail(self.subject, message, settings.EMAIL_HOST_USER, emails, html_message=message)
